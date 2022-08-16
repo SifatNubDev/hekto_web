15e2365d7788b0815142667434bb32a41faa9bfe
slothwit17 <rakibuzzamansiam@gmail.com>
2022-08-16 21:19:41 +0600

    faild to add slider in banner

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+$(function(){
+// banner slider
+// $('.banner-slider').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: false,
+//       autoplaySpeed: 2000,
+//       arrows: false,
+//       centerMode: true,
+//       centerPadding:0,
+//       responsive: [
+//           {
+//             breakpoint: 576,
+//             settings: {
+//               slidesToShow: 1,
+//             }
+//           },
+//           {
+//               breakpoint: 768,
+//               settings: {
+//                 slidesToShow: 2,
+//                 centerMode:false,
+//               }
+//             }
+//         ]
+//   });
+  });
