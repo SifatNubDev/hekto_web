c6c800a0e8860c50ddb819ad4c72d6dbebf6f90b
slothwit17 <rakibuzzamansiam@gmail.com>
2022-08-28 12:35:56 +0600

    complete

--- js/custom.js
+++ js/custom.js
@@ -47,4 +47,33 @@ $('.featured-product-slider').slick({
             }
         ]
   });
+
+
+  // top-categories-slider
+  $('.top-categories-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: true,
+    centerMode: false,
+    centerPadding:0,
+    responsive: [
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+            }
+          },
+          {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                centerMode:false,
+              }
+            }
+        ]
+  });
+
   });
