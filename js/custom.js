8a923cb95e11564393cfdfaac01c23ee95ba2233
slothwit <rakibuzzaman16-408@diu.edu.bd>
2022-09-14 00:36:48 +0600

    shopping cart

--- js/custom.js
+++ js/custom.js
@@ -84,25 +84,74 @@ $('.featured-product-slider').slick({
 ///////////-------------------quantity---------------////////////////
 
 
-function wcqib_refresh_quantity_increments() {
-  jQuery("div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)").each(function(a, b) {
-      var c = jQuery(b);
-      c.addClass("buttons_added"), c.children().first().before('<input type="button" value="-" class="minus" />'), c.children().last().after('<input type="button" value="+" class="plus" />')
-  })
+function updatedQuantity(inputField, isIncrease, single, total){
+  let numberField = document.getElementById(inputField);
+  const numberString = numberField.value;
+  const previousNumber = parseInt(numberString);
+
+
+  const singleUnit = document.getElementById(single);
+  const totalUnit = document.getElementById(total);
+  const singleString = singleUnit.innerText;
+  const previousSingle = parseFloat(singleString);
+  
+  
+  
+
+  let newNumber;
+
+  if(isIncrease === true){
+      newNumber = previousNumber + 1;
+  }
+  else{
+    if(previousNumber>1){
+      newNumber = previousNumber - 1;
+    }
+    else{
+      newNumber = 1;
+    }
+  }
+
+  numberField.value = newNumber;
+
+  let totalUnitPrice = previousSingle * newNumber;
+  totalUnit.innerText = totalUnitPrice.toFixed(2);
+  return totalUnitPrice;
 }
-String.prototype.getDecimals || (String.prototype.getDecimals = function() {
-  var a = this,
-      b = ("" + a).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
-  return b ? Math.max(0, (b[1] ? b[1].length : 0) - (b[2] ? +b[2] : 0)) : 0
-}), jQuery(document).ready(function() {
-  wcqib_refresh_quantity_increments()
-}), jQuery(document).on("updated_wc_div", function() {
-  wcqib_refresh_quantity_increments()
-}), jQuery(document).on("click", ".plus, .minus", function() {
-  var a = jQuery(this).closest(".quantity").find(".qty"),
-      b = parseFloat(a.val()),
-      c = parseFloat(a.attr("max")),
-      d = parseFloat(a.attr("min")),
-      e = a.attr("step");
-  b && "" !== b && "NaN" !== b || (b = 0), "" !== c && "NaN" !== c || (c = ""), "" !== d && "NaN" !== d || (d = 0), "any" !== e && "" !== e && void 0 !== e && "NaN" !== parseFloat(e) || (e = 1), jQuery(this).is(".plus") ? c && b >= c ? a.val(c) : a.val((b + parseFloat(e)).toFixed(e.getDecimals())) : d && b <= d ? a.val(d) : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())), a.trigger("change")
-});
+
+document.getElementById('selected-product-main').addEventListener('click', function(event){
+  let btn = event.target.id;
+  var array1 = [];
+  var array2 = [];
+  var array3 = [];
+  if(btn == 'minus-btn1'){
+    var result1 = updatedQuantity('quantity1', false, 'singleUnit1', 'totalUnit1');
+    array1.push(result1);
+  }
+  else if(btn == 'plus-btn1'){
+    var result1 = updatedQuantity('quantity1', true, 'singleUnit1', 'totalUnit1');
+    array1.push(result1);
+  }
+  else if(btn == 'minus-btn2'){
+    var result1 = updatedQuantity('quantity2', false, 'singleUnit2', 'totalUnit2');
+    array2.push(result1);
+  }
+  else if(btn == 'plus-btn2'){
+    var result1 = updatedQuantity('quantity2', true, 'singleUnit2', 'totalUnit2');
+    array2.push(result1);
+  }
+  else if(btn == 'minus-btn3'){
+    var result1 = updatedQuantity('quantity3', false, 'singleUnit3', 'totalUnit3');
+    array3.push(result1);
+  }
+  else if(btn == 'plus-btn3'){
+    var result1 = updatedQuantity('quantity3', true, 'singleUnit3', 'totalUnit3');
+    array3.push(result1);
+  }
+  
+})
+
+
+
+
+
